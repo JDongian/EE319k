b8778e53be89feccb169cc6f1f22d012518bedb7
Mukund Sunder Kidambi <mukkid@gmail.com>
2013-04-02 14:25:39 -0500

    fixed enables

--- Lab7/DAC.c
+++ Lab7/DAC.c
@@ -2,10 +2,10 @@
 #include <lm3s1968.h>
 
 void DAC_Init(void){
-	GPIO_PORTF_DIR_R |= 0x04;
-	GPIO_PORTF_AFSEL_R &= ~0x04;
-	GPIO_PORTF_DEN_R |= 0x04;
-	//GPIO_PORTF_PUR_R |= 0x04;
+	GPIO_PORTF_DIR_R |= 0x07;
+	GPIO_PORTF_AFSEL_R &= ~0x07;
+	GPIO_PORTF_DEN_R |= 0x07;
+	//GPIO_PORTF_PUR_R |= 0x07;
 	
 }
 void DAC_Out(char amplitude){
