7a6196c6c6bdbe36a489fb711ad09b986199673d
Mukund Sunder Kidambi <mukkid@gmail.com>
2013-04-01 23:40:21 -0500

    Sine function;  working on sine wave output

--- Lab7/Sound.c
+++ Lab7/Sound.c
@@ -1,19 +1,28 @@
 #include <math.h>
 #include <lm3s1968.h>
 #define TAO 6.283185307179586
-#define TABLE_SIZE 80000
+#define TABLE_SIZE 100
+
+char table[TABLE_SIZE];
 void Sound_Init() 
 {
-	char table[TABLE_SIZE];
+	
 	int i;
 	for(i = 0; i<TABLE_SIZE; i++)
 	{
-		table[i] = sin(TAO*i/TABLE_SIZE);
+		table[i] = 7*sin(TAO*i/TABLE_SIZE);
 	}
 }
 
 void Sound_Play(unsigned char n)
 {
-	//play
+	int delay = 1/(TABLE_SIZE*n);
+	int i;
+	while(i < TABLE_SIZE)
+	{
+		DAC_Out(table[i]);
+	}
+	
+	
 }
 
