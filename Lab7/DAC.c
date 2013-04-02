07655a2628ba36eb784602c8bb2f9dbf1eaa26d9
Mukund Sunder Kidambi <mukkid@gmail.com>
2013-04-02 13:06:52 -0500

    DAC updated

--- Lab7/DAC.c
+++ Lab7/DAC.c
@@ -2,11 +2,14 @@
 #include <lm3s1968.h>
 
 void DAC_Init(void){
-//	GPIO_PORTF_DIR_R = 
+	GPIO_PORTF_DIR_R |= 0x04;
+	GPIO_PORTF_AFSEL_R &= ~0x04;
+	GPIO_PORTF_DEN_R |= 0x04;
+	//GPIO_PORTF_PUR_R |= 0x04;
 	
 }
 void DAC_Out(char amplitude){
-//	GPIO_PORTF_DIR_R = 
+	GPIO_PORTF_DIR_R = amplitude ;
 }
 	
 
