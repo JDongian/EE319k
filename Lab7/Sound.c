e957670e745798146f55a062be8e23b435e434ed
Joshua Dong <jdong42@gmail.com>
2013-04-03 16:24:47 -0500

    new implementation using interrupts resubmitted

--- Lab7/Sound.c
+++ Lab7/Sound.c
@@ -1,52 +1,36 @@
-
 #include "Sound.h"
-#include "DAC.h"
-#include <math.h>
-#include <lm3s1968.h>
 #include "SysTickInts.h"
-#define TAO 6.283185307179586
-#define TABLE_SIZE 100
 
-char table[TABLE_SIZE];
-void Sound_Init() 
-{
-	int i;
-	for(i = 0; i<TABLE_SIZE; i++)
-	{
-		table[i] = 4.0*sin(TAO*i/TABLE_SIZE)+4.0;
-	}
-}
+char waveform[TABLE_SIZE] = {0};
+volatile int sampleIndex = 0;
+int tempo = 120;
 
-void Sound_Play(unsigned short freq)
-{
-	int delay = 1/(TABLE_SIZE*freq);
+void Sound_Init() {
+	//Global Variables
 	int i = 0;
-	if (freq == 0){
-		DAC_Out(0);
-		return;
-	}
-	while(i < TABLE_SIZE)
-	{
-		DAC_Out(table[i]);
-		Delay(delay);
-		i++;
-	}
+	for(; i<TABLE_SIZE; i++) {
+		waveform[i] = 4.0*sin(TAO*i/TABLE_SIZE)+4.0;
+	}	
 }
 
-void Demo(){
-	Sound_Play(E4);
-	Sound_Play(Off);
-	Sound_Play(D4);
-	Sound_Play(Off);
-	Sound_Play(C4);
-	Sound_Play(Off);
-	Sound_Play(D4);
-	Sound_Play(D4);
+void Sound_Play(unsigned short freq) {
+	double period = 1/(TABLE_SIZE*freq);
+	sampleIndex = 0;
+	Set_SysTick_Period(period);
 }
 
-void Delay(int microseconds){
-	
-	long double curTime = SYS_TIME;
-	while(SYS_TIME - microseconds != curTime){}
-	return;
+void Sound_Play_Note(note n) {
+	double i;
+	if (n.frequency == 0) { Sound_Play(Off);
+	} else { Sound_Play(n.frequency); }
+	for (i = 0.0; i < n.time*1000000/tempo; i++) {}
+	Sound_Play(Off);
+	for (i = 0.0; i < 10; i++) {}
+}
+
+void Sound_Play_Song(note song[]) {
+	int i = 0;
+	for (; song[i].time != 0; ++i) {
+			Sound_Play_Note(song[i]);
+	}
 }
