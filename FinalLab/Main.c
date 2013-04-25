7eed2eb4f69b87a01352bb35a888a3f8cc614c7a
Joshua Dong <jdong42@gmail.com>
2013-04-24 22:56:46 -0500

    removed Valvano

--- FinalLab/Main.c
+++ FinalLab/Main.c
@@ -10,7 +10,7 @@ int main(void){
 	SysTick_IntEnable();
 	while(1) {
 		if(HWREGBITW(&gFlags, FRAME_BUFFER_READY) == 0) { continue; }
-		clearBuffer();
+		clearBuffer();		
 		demo();
 		HWREGBITW(&gFlags, FRAME_BUFFER_READY) = 1;
 	}
