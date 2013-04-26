1a23ac8cfcea15012ba623adfef92d398da963a0
JDong820 <jdong42@gmail.com>
2013-04-26 12:28:21 -0500

    Added comments and updated header for graphicsUtil.
    frameBuffer may be moved to globals.h. Probably not though, on second thought.

--- FinalLab/drawLib/graphicsUtil.h
+++ FinalLab/drawLib/graphicsUtil.h
@@ -1,21 +1,19 @@
+#include <strings.h>
 #include "hw_types.h"
 #include "math2.h"
 
-//Driver interfacing
-unsigned char* getBuffer(void);
-void clearBuffer(void);
-void drawPx(point, unsigned char);
-//Misc
-void intSwap (int*, int*);
-int crossP(point, point);
-box getBox(point*, int);
-bool pointInPolygon(point*, int, point);
-//Point functions
-point makePoint(int, int);
-void addPoint(point*, int, int);//DONOT USE		//Inserts a point to a {-1, -1} terminated array.
-//pointArr getLine(point, point);
-//pointArr getRect(point, point);
-//pointArr getCircle(point, int);
-//pointArr rotate(point, char, pointArr);
-//pointArr scale(point, float, pointArr);
 
+//Buffer interface functions
+unsigned char* getBuffer(void);   //Returns the current frameBuffer.
+void clearBuffer(void);   //Sets frameBuffer to black.
+void drawPx(point, unsigned char);   //Writes to the frameBuffer.
+//PENDING MOVE///////////////////////////
+void intSwap (int*, int*);
+point makePoint(int, int);
+int crossP(point, point);   //Cross product with respect to the origin
+bool isBetween(int, int, int);   //Endpoint inclusive bound check
+bool lineIntersect(point, point, point, point);   //**Reliable in case a is horizantal. **BUGGED:a1**
+///////////////////////////////////////////
+//Collision detect helpers
+box getBox(point*, int);  //Finds the minimum fitting box.
+bool pointInPolygon(point*, int, point);   //Uses a ray-pairity implemetation.
