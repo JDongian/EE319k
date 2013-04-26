7e1c4fa0f31c0548c949bd0500813159a9e229b8
Joshua Dong <jdong42@gmail.com>
2013-04-26 12:53:19 -0500

    added some code to test later for pointInPolygon
    http://stackoverflow.com/questions/11716268/point-in-polygon-algorithm

--- FinalLab/drawLib/graphicsUtil.c
+++ FinalLab/drawLib/graphicsUtil.c
@@ -96,6 +96,22 @@ bool pointInPolygon(point* verticies, int numberOfVerticies, point test) {
 	} return False;
 }
 /*
+http://stackoverflow.com/questions/11716268/point-in-polygon-algorithm
+int pnpoly(int nvert, float *vertx, float *verty, float testx, float testy)
+{
+  int i, j, c = 0;
+  for (i = 0, j = nvert-1; i < nvert; j = i++) {
+    if ( ((verty[i]>testy) != (verty[j]>testy)) &&
+     (testx < (vertx[j]-vertx[i]) * (testy-verty[i]) / (verty[j]-verty[i]) + vertx[i]) )
+       c = !c;
+  }
+  return c;
+}
+*/
+
+
+
+/*
 pointArr scale(point center, float scaleFactor, pointArr obj){
 	pointArr scaleObj; int i = 0;
 	while(obj.array[i].x != END) {
